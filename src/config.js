7037a462599f2913d51b1e29f0bfe5b99ec35ce6
Sashco Mistelbacher <sashco.mistelbacher@gmail.com>
2021-04-25 14:27:22 -0400

    setting up react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "sashcoimages"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://gbf3u98iwa.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_Wr2V52pet",
+      APP_CLIENT_ID: "5qa9pv6p2nc1mg6cdpv9as4mum",
+      IDENTITY_POOL_ID: "us-east-2:b46c527a-67d5-4008-bbaa-0623a9c8a722"
+    }
+  };
