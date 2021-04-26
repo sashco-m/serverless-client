4da2e78d1b1f47d85ec4fe22f3afd47326057b5a
Sashco Mistelbacher <sashco.mistelbacher@gmail.com>
2021-04-26 17:19:07 -0400

    completed adding and removing notes functionality:

--- src/containers/NewNote.js
+++ src/containers/NewNote.js
@@ -7,6 +7,7 @@ import config from "../config";
 import "./NewNote.css";
 
 import { API } from "aws-amplify";
+import { s3Upload } from "../libs/awsLib";
 
 export default function NewNote() {
   const file = useRef(null);
@@ -27,8 +28,9 @@ export default function NewNote() {
 
     if (file.current && file.current.size > config.MAX_ATTACHMENT_SIZE) {
       alert(
-        `Please pick a file smaller than ${config.MAX_ATTACHMENT_SIZE /
-          1000000} MB.`
+        `Please pick a file smaller than ${
+          config.MAX_ATTACHMENT_SIZE / 1000000
+        } MB.`
       );
       return;
     }
@@ -36,13 +38,16 @@ export default function NewNote() {
     setIsLoading(true);
 
     try {
-      await createNote({ content });
+      const attachment = file.current ? await s3Upload(file.current) : null;
+
+      await createNote({ content, attachment });
       history.push("/");
     } catch (e) {
       onError(e);
       setIsLoading(false);
     }
   }
+
 
   function createNote(note) {
     return API.post("notes", "/notes", {
